32731bb64c1dd77f1771f781892ad170a2d4d7db
chen nan <pandachennan@seedyee.com>
2016-11-11 00:26:13 +0800

    add two forms what username and passowrd modification in account page

--- src/shared/modules/SettingsPage/Account/index.js
+++ src/shared/modules/SettingsPage/Account/index.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import Styles from './index.css'
+import FormInputField from '../../FormInputField'
+import { Button } from 'react-bootstrap'
 
-const Account = () => (
-  <div>
-    <h1>Account</h1>
-  </div>
-)
+class Account extends Component {
+  constructor() {
+    super()
+    this.state = {
+      redirectTo: null,
+    }
+  }
 
-export default Account
+  render() {
+    const { handleSubmit } = this.props
+    return (
+      <div className={Styles.Account}>
+        <div className={Styles.userName}>
+          <h3>修改用户名</h3>
+          <form onSubmit={handleSubmit}>
+            <Field
+              styles={{ input: Styles.input }}
+              name="username"
+              type="text"
+              id="username"
+              component={FormInputField}
+            />
+            <Button bsStyle="default" className={Styles.submitBtn} type="submit">更新用户名</Button>
+          </form>
+        </div>
+        <div className={Styles.passwordEdit}>
+          <h3>修改密码</h3>
+          <form onSubmit={handleSubmit}>
+            <Field
+              styles={{ input: Styles.input }}
+              name="oldPassword"
+              type="text"
+              id="oldPassword"
+              component={FormInputField}
+              label="当前密码"
+            />
+            <Field
+              styles={{ input: Styles.input }}
+              name="newPassword"
+              type="text"
+              id="newPassword"
+              component={FormInputField}
+              label="新密码"
+            />
+            <Field
+              styles={{ input: Styles.input }}
+              name="newPasswordConfirm"
+              type="text"
+              id="newPasswordConfirm"
+              component={FormInputField}
+              label="确认密码"
+            />
+            <Button bsStyle="default" className={Styles.submitBtn} type="submit">更新密码</Button>
+          </form>
+        </div>
+      </div>
+
+    )
+  }
+
+}
+
+export default reduxForm(
+
+)(Account)
 
