9ac3e9e0446f91722d5fcd9bfab95d020a7c2da7
chen nan <chennan@leapcorn.com>
2016-12-21 15:44:01 +0800

    Add website keywords and description

--- src/shared/modules/App/index.js
+++ src/shared/modules/App/index.js
@@ -18,7 +18,7 @@ import ProfilePage from '../ProfilePage'
 import SettingsPage from '../SettingsPage'
 import ProjectPage from '../ProjectPage'
 
-const websiteDescription = 'A NodeJS V6 Universal React Boilerplate with an Amazing Developer Experience.'
+const websiteDescription = '深圳习得易网络有限公司是致力于为广大的互联网开发者打造一站式自动化互联网软件服务平台。'
 
 if (process.env.NODE_ENV === 'development') {
   console.log('ES Modules Supported:', camelCase('hello-world') === 'helloWorld')
@@ -34,6 +34,7 @@ const App = () => (
         { name: 'charset', content: 'utf-8' },
         { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'keywords', content: 'CDE, cde.io, 习得易, node-matrix, 可视化编程项目托管, 可视化编程组件, 一键部署, 项目管理, 团队协作' },
         { name: 'description', content: websiteDescription },
       ]}
     />
